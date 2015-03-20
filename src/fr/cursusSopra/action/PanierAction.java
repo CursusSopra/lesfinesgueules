d93b2bb98f512194b55d17e832385b35cd3db095
Daniel Lucazeau <contact@dlucazeau.fr>
2015-03-20 14:33:47 +0100

    Retour arrière sur annotations

--- src/fr/cursusSopra/action/PanierAction.java
+++ src/fr/cursusSopra/action/PanierAction.java
@@ -1,9 +1,9 @@
 package fr.cursusSopra.action;
 
-import org.apache.struts2.convention.annotation.Result;
-import org.apache.struts2.convention.annotation.Results;
-
-@Results({ @Result(name = "success", type="tiles", location = "index") })
+//import org.apache.struts2.convention.annotation.Result;
+//import org.apache.struts2.convention.annotation.Results;
+//
+//@Results({ @Result(name = "success", type="tiles", location = "index") })
 public class PanierAction extends ActionSupportExtended {
 
 	/**
