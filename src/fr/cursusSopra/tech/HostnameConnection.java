841385ee3ec0453eee2986ace2fca0f033d49c0d
Julien Caillon <julien.caillon@gmail.com>
2015-03-20 10:32:45 +0100

    Ajout DataLayerExtended
    Faites un "extends DataLayerExtended" dans toutes vos classes DAL pour
    hériter de "connection"

--- src/fr/cursusSopra/tech/HostnameConnection.java
+++ src/fr/cursusSopra/tech/HostnameConnection.java
@@ -1,13 +1,17 @@
 package fr.cursusSopra.tech;
 
 public class HostnameConnection {
+	
+	// Static car partagee par toutes les instances de this
 	private static HostnameConnection instance = null;
-
 	private String hostName = "";
 
+	// constructeur private, accessible uniquement par la classe elle m�me
 	private HostnameConnection() {
 	}
 
+	// getInstance(), c'est une methode PUBLIC, c'est par cette methode qu'on instancie l'objet unique "HostnameConnection"
+	// C'est une methode STATIC car cette methode doit etre executable sans avoir besoin d'instancier this
 	public static HostnameConnection getInstance() {
 		if (instance == null) {
 			instance = new HostnameConnection();
