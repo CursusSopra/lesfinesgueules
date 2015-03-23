e205ec8cb66a6b0456383f4bb41a8adeb0725e75
Cécile Bernard <cecile.asd@gmail.com>
2015-03-23 10:57:39 +0100

    utilisateur fonctionnel web-metier-bdd

--- src/fr/cursusSopra/action/utilisateurs/InscriptionAction.java
+++ src/fr/cursusSopra/action/utilisateurs/InscriptionAction.java
@@ -60,19 +60,16 @@ public class InscriptionAction extends ActionSupportExtended{
 		return SUCCESS;
 	}
 	
-	
 	//ajout utilisateur en BDD
 	public String createProfil() throws SQLException {
 		
-		Utilisateur utilisateur = new Utilisateur(idUtilisateur, nom, prenom, ligneAdresse1, ligneAdresse2,
+		Utilisateur utilisateur = new Utilisateur(nom, prenom, ligneAdresse1,
 				codePostal, ville, email, mdp, tel, photo, droits);
 		
 		utilisateur.save();
 		
 		return SUCCESS;
-		
 	}
-	
 	
 
 }
