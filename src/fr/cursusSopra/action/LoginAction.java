b0f926bcaf04c24b89e12faa9e56b62807d7af73
Daniel Lucazeau <contact@dlucazeau.fr>
2015-03-26 09:50:17 +0100

    Protection par authentification de parties de l'application

--- src/fr/cursusSopra/action/LoginAction.java
+++ src/fr/cursusSopra/action/LoginAction.java
@@ -3,7 +3,11 @@ package fr.cursusSopra.action;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.apache.struts2.interceptor.ServletRequestAware;
+
+import fr.cursusSopra.action.admin.ProducteurAction;
 
 public class LoginAction extends ActionSupportExtended implements
 		ServletRequestAware {
@@ -11,11 +15,15 @@ public class LoginAction extends ActionSupportExtended implements
 	 * 
 	 */
 	private static final long serialVersionUID = 3771868804107524884L;
+	
+	private static final Logger logger = LogManager
+			.getLogger(LoginAction.class);
 
 	private HttpServletRequest request;
 
 	private String email;
 	private String mdp;
+	private String savedURL;
 
 	public String getEmail() {
 		return email;
@@ -29,15 +37,22 @@ public class LoginAction extends ActionSupportExtended implements
 		this.mdp = mdp;
 	}
 
+	public String getSavedURL() {
+		return savedURL;
+	}
+
 	public String execute() {
 		return SUCCESS;
 	}
 
 	public String control() {
+		HttpSession session = request.getSession();
 
 		if (email.equals("mail@mail.fr")) {
-			HttpSession session = request.getSession();
 			session.setAttribute("authorized", "yes");
+			savedURL = session.getAttribute("savedUrl").toString();
+			//savedURL = "/views/welcome.jsp";
+			logger.info(savedURL);
 			return SUCCESS;
 		}
 
--- src/fr/cursusSopra/tech/AuthInterceptor.java
+++ src/fr/cursusSopra/tech/AuthInterceptor.java
@@ -2,10 +2,22 @@ package fr.cursusSopra.tech;
 
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.apache.struts2.ServletActionContext;
+
+import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionInvocation;
 import com.opensymphony.xwork2.interceptor.Interceptor;
 
+import fr.cursusSopra.action.LoginAction;
+
 public class AuthInterceptor implements Interceptor {
+	private static final Logger logger = LogManager
+			.getLogger(AuthInterceptor.class);
 
 	@Override
 	public void destroy() {
@@ -21,8 +33,21 @@ public class AuthInterceptor implements Interceptor {
 
 	@Override
 	public String intercept(ActionInvocation invocation) throws Exception {
+		// Mémorisation de l'action demandée
+		ActionContext context = invocation.getInvocationContext();
+	    HttpServletRequest request = (HttpServletRequest) context.get(ServletActionContext.HTTP_REQUEST);
+
+		String queryString = request.getQueryString();
+		HttpSession session = request.getSession();
+		session.setAttribute("savedUrl", request.getRequestURI()
+				+ (queryString == null ? "" : ("?" + queryString)));
+		
+		logger.info(request.getRequestURI());
+		
+
 		Map<String, Object> sessionAttributes = invocation
 				.getInvocationContext().getSession();
+		
 		if (sessionAttributes == null
 				|| sessionAttributes.get("authorized") == null) {
 			return "failure";
