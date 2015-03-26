4a57184c48c0d3050b8190e79977774c0f1ec562
Benoît Lacroix <benoit.lacroix@outlook.com>
2015-03-26 17:10:04 +0100

    Choix nb items dans liste produits

--- WebContent/_js/recherche.js
+++ WebContent/_js/recherche.js
@@ -1,129 +1,217 @@
 /*
  * @author Benoît
- * 
- * Fichier de fonctions javascript pour l'affichage de la liste des produits
  */
 
+// Chargement de la page
 $(function() {
-	$.getJSON('getProduitsJSON?idType1=' + $('#idType1').val() + '&idType2=' + $('#idType2').val() + '&idProducteur=' + $('#idProducteur').val(), function(data) {
-				
-		var output = '';
+	
+	$.getJSON('getProduitsJSON?idType1=' + $('#idType1').val() + 
+			'&idType2=' + $('#idType2').val() + 
+			'&idProducteur=' + $('#idProducteur').val() +
+			'&displayNumber=' + $('#displayNumber').val() +
+			'&pageNumber=' + $('#pageNumber').val(), function(data) {
 		
-		$.each(data.listeProduits, function(index, elt) {
-			
-			// Disponible / Non disponible
-			if(elt.disponible) {
-				var badgeDisponible =	'<p><span class="label label-success">Disponible</span></p>';
-				var enableAddCart	=	'<input type="hidden" id="idProduit' + index + '" value="' + elt.idProduit + '"/>' +
-										'<div class="input-group">' +
-											'<div class="input-group-btn">' +
-												'<button type="button" class="btn btn-default" id="idButton' + index + '">' +
-													'<small>Ajouter au panier</small>' +
-												'</button>' +
-												'<button type="button" class="btn btn-default" id="buttonPlus' + index + '">' +
-													'<span class="glyphicon glyphicon-plus"></span>' +
-												'</button>' +
-											'</div>' +
-											'<input type="text" class="form-control" id="idQuantite' + index + '" value="1" readonly/>' +
-											'<span class="input-group-btn">' +
-												'<button type="button" class="btn btn-default" id="buttonMoins' + index + '">' +
-													'<span class="glyphicon glyphicon-minus"></span>' +
-												'</button>' +
-											'</span>' +
-										'</div>';	// input-group
-			} else {
-				var badgeDisponible = 	'<p><span class="label label-danger">Non disponible</span></p>';
-				var enableAddCart	=	'<input type="hidden" id="idProduit' + index + '" value="' + elt.idProduit + '"/>' +
-										'<div class="input-group">' +
-											'<div class="input-group-btn">' +
-												'<button type="button" class="btn btn-default" id="idButton' + index + '" disabled="disabled">' +
-													'<small>Ajouter au panier</small>' +
-												'</button>' +
-												'<button type="button" class="btn btn-default" id="buttonPlus' + index + '" disabled="disabled">' +
-													'<span class="glyphicon glyphicon-plus"></span>' +
-												'</button>' +
-											'</div>' +
-											'<input type="text" class="form-control" id="idQuantite' + index + '" value="1" readonly/>' +
-											'<span class="input-group-btn">' +
-												'<button type="button" class="btn btn-default" id="buttonMoins' + index + '" disabled="disabled">' +
-													'<span class="glyphicon glyphicon-minus"></span>' +
-												'</button>' +
-											'</span>' +
-										'</div>';	// input-group
-			}
-			
-			var prixDetails =	'<div class="input-group">' +
-									'<span class="input-group-btn">' +
-										'<a  class="btn btn-default" href="detailsProduit.action?idProduit=' + elt.idProduit + '">' + // Bouton "Détails"
-											'<small>D&eacute;tails produit</small>' +
-										'</a>' +
-									'</span>' +
-									'<input type="text" class="form-control" value="' + $.number(elt.prix, 2, ',', ' ') + ' &euro;" readonly>' +
-								'</div>';
-			
-			// Ajout de l'image par défaut si non présente
-			if(elt.photo.length == 0){
-				elt.photo = 'images/default.jpg';
-			}
-			
-			// Création de la variable chaîne de sortie
-			output +=
-				'<div class="col-md-3">' + 
-					'<a href="detailsProduit.action?idProduit=' + elt.idProduit + '">' +
-						'<img alt="image" class="img-responsive img-thumbnail" id="idImage' + index + '" src="' + elt.photo + '"/>' + 
-					'</a>'+
-					
-					'<h4>' + elt.designation +
-						' <span class="badge">' + elt.listeCommentaires.length + ' avis</span>' + 
-					'</h4>' +
-					
-					badgeDisponible + // Label de disponibilité
-					
-					prixDetails +	// Affichage du prix et du bouton détails
-					
-					'<p>' + elt.description.substring(0,250) + '...</p>' + 
-					
-					enableAddCart + // Bouton ajout au panier
-					
-				'</div>' +	// col-md-3
-			'</div>';	// ???
-				
-		});
+		console.log('pageNumber = ' + $('#pageNumber').val());
+		console.log('displayNumber = ' + $('#displayNumber').val());
+		console.log('data.numberOfPages = ' + data.numberOfPages);
 		
-		// Mise du contenu dans la balise #listeProduits
-		$('#listeProduits').html(output);
-		
-		// Ajout évènement 'onclick' sur tous les boutons 'Ajout produit'
-		addClickEventPanier();
-		
-		// Increment the number of product (max 99)
-		$("button[id^='buttonPlus']").click(function () {
-			// Récupération de l'ID complet
-			var thisId = $(this).attr("id");
-			
-			var thisQuantite = $("#" + thisId.replace("buttonPlus","idQuantite"));
-			
-			if(thisQuantite.attr('value') < 99) {
-				thisQuantite.attr('value', parseInt(thisQuantite.attr('value')) + 1);
-			} else {
-				alert('Nombre de produit maximum atteint.')
-			}
-		});
-		
-		// Decrement the number of product (min 1)
-		$("button[id^='buttonMoins']").click(function () {
-			// Récupération de l'ID complet
-			var thisId = $(this).attr("id");
-			
-			var thisQuantite = $("#" + thisId.replace("buttonMoins","idQuantite"));
-			
-			if(thisQuantite.attr('value') > 1) {
-				thisQuantite.attr('value', parseInt(thisQuantite.attr('value')) - 1);
-			} else {
-				alert('Vous devez avoir au moins un produit.')
-			}
-		});
+		getPage(data);
 	});
 	
 });
 
+// Fonction JSON
+function getPage(data) {
+	
+	// Mise du contenu dans la balise #listeProduits
+	$('#listeProduits').html(displayList(data));
+	
+	// Ajout évènement 'onclick' sur tous les boutons 'Ajout produit'
+	addClickEventPanier();
+	
+	// Changement du nombre de produits affichés
+	$('#displayNumber').change(function () {
+		changeDisplay();
+	});
+	
+	// Aller à la page précédente
+	$('#pPage').unbind('click').click(function () {
+		previousPage();
+	});
+	
+	// Aller à la page suivante
+	$('#nPage').unbind('click').click(function () {
+		nextPage();
+	});
+	
+	// Increment the number of product (max 99)
+	$("button[id^='buttonPlus']").click(function () {
+		// Récupération de l'ID complet
+		var thisId = $(this).attr("id");
+		
+		var thisQuantite = $("#" + thisId.replace("buttonPlus","idQuantite"));
+		
+		if(thisQuantite.attr('value') < 99) {
+			thisQuantite.attr('value', parseInt(thisQuantite.attr('value')) + 1);
+		} else {
+			alert('Nombre de produit maximum atteint.')
+		}
+	});
+	
+	// Decrement the number of product (min 1)
+	$("button[id^='buttonMoins']").click(function () {
+		// Récupération de l'ID complet
+		var thisId = $(this).attr("id");
+		
+		var thisQuantite = $("#" + thisId.replace("buttonMoins","idQuantite"));
+		
+		if(thisQuantite.attr('value') > 1) {
+			thisQuantite.attr('value', parseInt(thisQuantite.attr('value')) - 1);
+		} else {
+			alert('Vous devez avoir au moins un produit.')
+		}
+	});
+	
+}
+
+// UTILITARY FUNCTIONS ========================================================================== //
+
+function changeDisplay() {
+	$.getJSON('getProduitsJSON?idType1=' + $('#idType1').val() + 
+			'&idType2=' + $('#idType2').val() + 
+			'&idProducteur=' + $('#idProducteur').val() +
+			'&displayNumber=' + $('#displayNumber').val() +
+			'&pageNumber=' + 1, function(data) {
+		
+		getPage(data);
+	});
+}
+
+function nextPage() {
+	if($('#pageNumber').val() < $('#numberOfPages').val()) {
+		var tempPageNumber = parseInt($('#pageNumber').val()) + 1;
+		$.getJSON('getProduitsJSON?idType1=' + $('#idType1').val() + 
+				'&idType2=' + $('#idType2').val() + 
+				'&idProducteur=' + $('#idProducteur').val() +
+				'&displayNumber=' + $('#displayNumber').val() +
+				'&pageNumber=' + tempPageNumber, function(data) {
+			
+			getPage(data);
+		});
+		
+		$('#pageNumber').val(tempPageNumber);
+		console.log('pageNumber = ' + tempPageNumber);
+	}
+}
+
+function previousPage() {
+	if(parseInt($('#pageNumber').val()) > 1) {
+		var tempPageNumber = parseInt($('#pageNumber').val()) - 1;
+		$.getJSON('getProduitsJSON?idType1=' + $('#idType1').val() + 
+				'&idType2=' + $('#idType2').val() + 
+				'&idProducteur=' + $('#idProducteur').val() +
+				'&displayNumber=' + $('#displayNumber').val() +
+				'&pageNumber=' + tempPageNumber, function(data) {
+			
+			getPage(data);
+		});
+		
+		$('#pageNumber').val(tempPageNumber);
+		console.log('pageNumber = ' + tempPageNumber);
+	}
+}
+
+// DISPLAY FUNCTION ============================================================================= //
+
+function displayList(data) {
+	var output = '';
+	
+	// Ecriture des propriété d'affichage dans les champs cachés
+	$('#numberOfPages').val(data.numberOfPages);
+	$('#pageNumber').val(data.pageNumber);
+	
+	// Affichage de la navigation des pages
+	$('#nbPages').html(data.pageNumber + '/' + data.numberOfPages);
+	
+	// Boucle de création de la liste
+	$.each(data.listeProduits, function(index, elt) {
+		// Disponible / Non disponible
+		if(elt.disponible) {
+			var badgeDisponible =	'<p><span class="label label-success">Disponible</span></p>';
+			var enableAddCart	=	'<input type="hidden" id="idProduit' + index + '" value="' + elt.idProduit + '"/>' +
+									'<div class="input-group">' +
+										'<div class="input-group-btn">' +
+											'<button type="button" class="btn btn-default" id="idButton' + index + '">' +
+												'<small>Ajouter au panier</small>' +
+											'</button>' +
+											'<button type="button" class="btn btn-default" id="buttonPlus' + index + '">' +
+												'<span class="glyphicon glyphicon-plus"></span>' +
+											'</button>' +
+										'</div>' +
+										'<input type="text" class="form-control" id="idQuantite' + index + '" value="1" readonly/>' +
+										'<span class="input-group-btn">' +
+											'<button type="button" class="btn btn-default" id="buttonMoins' + index + '">' +
+												'<span class="glyphicon glyphicon-minus"></span>' +
+											'</button>' +
+										'</span>' +
+									'</div>';	// input-group
+		} else {
+			var badgeDisponible = 	'<p><span class="label label-danger">Non disponible</span></p>';
+			var enableAddCart	=	'<input type="hidden" id="idProduit' + index + '" value="' + elt.idProduit + '"/>' +
+									'<div class="input-group">' +
+										'<div class="input-group-btn">' +
+											'<button type="button" class="btn btn-default" id="idButton' + index + '" disabled="disabled">' +
+												'<small>Ajouter au panier</small>' +
+											'</button>' +
+											'<button type="button" class="btn btn-default" id="buttonPlus' + index + '" disabled="disabled">' +
+												'<span class="glyphicon glyphicon-plus"></span>' +
+											'</button>' +
+										'</div>' +
+										'<input type="text" class="form-control" id="idQuantite' + index + '" value="1" readonly/>' +
+										'<span class="input-group-btn">' +
+											'<button type="button" class="btn btn-default" id="buttonMoins' + index + '" disabled="disabled">' +
+												'<span class="glyphicon glyphicon-minus"></span>' +
+											'</button>' +
+										'</span>' +
+									'</div>';	// input-group
+		}
+		
+		var prixDetails =	'<div class="input-group">' +
+								'<span class="input-group-btn">' +
+									'<a  class="btn btn-default" href="detailsProduit.action?idProduit=' + elt.idProduit + '">' + // Bouton "Détails"
+										'<small>D&eacute;tails produit</small>' +
+									'</a>' +
+								'</span>' +
+								'<input type="text" class="form-control" value="' + $.number(elt.prix, 2, ',', ' ') + ' &euro;" readonly>' +
+							'</div>';
+		
+		// Ajout de l'image par défaut si non présente
+		if(elt.photo.length == 0){
+			elt.photo = 'images/default.jpg';
+		}
+		
+		// Création de la variable chaîne de sortie
+		output +=
+			'<div class="col-md-3">' + 
+				'<a href="detailsProduit.action?idProduit=' + elt.idProduit + '">' +
+					'<img alt="image" class="img-responsive img-thumbnail" id="idImage' + index + '" src="' + elt.photo + '"/>' + 
+				'</a>'+
+				
+				'<h4>' + elt.designation +
+					' <span class="badge">' + elt.listeCommentaires.length + ' avis</span>' + 
+				'</h4>' +
+				
+				badgeDisponible + // Label de disponibilité
+				
+				prixDetails +	// Affichage du prix et du bouton détails
+				
+				'<p>' + elt.description.substring(0,250) + '...</p>' + 
+				
+				enableAddCart + // Bouton ajout au panier
+				
+			'</div>' +	// col-md-3
+		'</div>';	// ???
+	});
+	
+	return output;
+}
--- src/fr/cursusSopra/json/JSONListeProduitsAction.java
+++ src/fr/cursusSopra/json/JSONListeProduitsAction.java
@@ -3,7 +3,6 @@
  */
 package fr.cursusSopra.json;
 
-
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,25 +13,68 @@ import fr.cursusSopra.model.Produit;
 
 public class JSONListeProduitsAction {
 	
+	/* FILTER PROPERTIES */
+	
 	private long idType1 = -1;
 	private long idType2 = -1;
 	private long idProducteur = -1;
+	
+	/* DISPLAY PROPERTIES */
+	
+	private int displayNumber = 0;
+	private int pageNumber = 1;
+	private int numberOfPages = 1;
+	
+	/* OUTPUT LIST */
+	
 	private List<Produit> listeProduits = new ArrayList<>();
 
 	public JSONListeProduitsAction() throws SQLException {
-//		System.out.println("idType1 = " + idType1);
-//		System.out.println("idType2 = " + idType2);
-//		System.out.println("idProducteur = " + idProducteur);
 	}
-
+	
+	/* EXECUTE METHOD */
+	
 	public String execute() {
 		try {
 			setListeProduits(Produit.getListeProduits(idType1, idType2, idProducteur));
 		} catch (Exception e) {
 			System.out.println("getListeProduits impossible.");
 		}
+		
+		filterDisplay();
+		
+		System.out.println("pageNumber = " + pageNumber);
+		System.out.println("numberOfPages = " + numberOfPages);
+		System.out.println("displayNumber = " + displayNumber);
+		
+		
 		return Action.SUCCESS;
 	}
+	
+	/* METHODS */
+	
+	private void filterDisplay() {
+		int lpLength = listeProduits.size();
+		numberOfPages = (int) Math.ceil(((double) lpLength) / ((double) displayNumber));
+		System.out.println("===================================");
+		System.out.println("lpLength = " + lpLength);
+		System.out.println("displayNumber = " + displayNumber);
+		System.out.println("numberOfPages = " + numberOfPages);
+		System.out.println("===================================");
+		
+		int minProduitIndex = (pageNumber - 1) * displayNumber;
+		int maxProduitIndex = (pageNumber * displayNumber) > lpLength ? lpLength : pageNumber * displayNumber;
+		
+		List<Produit> tempList = new ArrayList<Produit>();
+		
+		for (int i = minProduitIndex; i < maxProduitIndex; i++) {
+			tempList.add(listeProduits.get(i));
+		}
+		
+		listeProduits = tempList;
+	}
+	
+	/* OUTPUT LIST */
 
 	public List<Produit> getListeProduits() {
 		return listeProduits;
@@ -41,6 +83,8 @@ public class JSONListeProduitsAction {
 	private void setListeProduits(List<Produit> listeProduits) {
 		this.listeProduits = listeProduits;
 	}
+	
+	/* FILTER ACCESSORS */
 	
 	public void setIdType1(String idType1) {
 		try {
@@ -71,6 +115,42 @@ public class JSONListeProduitsAction {
 			this.idProducteur = -1;
 			System.out.println("Conversion String > Long impossible pour idProducteur.");
 		}
+	}
+	
+	/* DISPLAY ACCESSORS */
+	
+	public void setDisplayNumber(String displayNumber) {
+		try {
+			this.displayNumber = Integer.parseInt(displayNumber);
+			System.out.println("Conversion String > Int faite pour displayNumber | displayNumber = " + this.displayNumber);
+		} catch (NumberFormatException e) {
+			this.displayNumber = 0;
+			e.printStackTrace();
+			System.out.println("Conversion String > Int impossible pour displayNumber.");
+		}
+	}
+	
+	public void setPageNumber(String pageNumber) {
+		try {
+			this.pageNumber = Integer.parseInt(pageNumber);
+			System.out.println("Conversion String > Int faite pour pageNumber | pageNumber = " + this.pageNumber);
+		} catch (NumberFormatException e) {
+			this.pageNumber = 0;
+			e.printStackTrace();
+			System.out.println("Conversion String > Int impossible pour pageNumber.");
+		}
+	}
+	
+	public int getPageNumber() {
+		return pageNumber;
+	}
+	
+	public int getDisplayNumber() {
+		return displayNumber;
+	}
+	
+	public int getNumberOfPages() {
+		return numberOfPages;
 	}
 	
 }
