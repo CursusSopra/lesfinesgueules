ab9c1b421fd4ffb414d062cb11228d88583240d4
Julien Caillon <julien.caillon@gmail.com>
2015-03-27 15:16:52 +0100

    maj script sql pour les charcuteries

--- dossier/create_schema.sql
+++ dossier/create_schema.sql
@@ -239,16 +239,16 @@ Très apprécié sur le plateau d''un repas de fête...', 1.56, 'Charolais', 'Ch
 (1, 3, 'C''est un petit fromage à pâte molle à croûte fleurie et de couleur blanche, jaune ou bleue selon sa maturité2. Il contient entre 40 et 45 % de matière grasse.', 1.50, 'Bouton de culotte', 'bouton_de_culotte.jpg', false);
 
 INSERT INTO produits (id_producteur, id_type2, description, prix, designation, photo, disponible) VALUES 
-(1, 1, 'Terrine de foie gras de canard. Se déguste avec un vin moelleux ou du beaujolais. Conditionné ainsi il se conserve 2 ans.', 19.50, 'Terrine de foie gras, 190gr', 'foiegras.jpg', true),
-(1, 1, 'Ce magret de canard de race Mulard est farci au foie gras. Il est idéal en salade, avec un vin moelleux. Il se conserve 40 jours.', 14.80, 'Magret de canard au foie gras (35%),150gr', 'magret.jpg', false),
-(2, 1, 'Nos rillettes de canard se dégustent avec un vin rouge léger. Elles contiennent 20% de foie gras. La conservation est de 30 jours.', 8.70, 'Rillettes de canard', 'rillettes.jpg', true),
-(1, 1, 'Foie gras de canard entier aux pépites de figues séchées. Les Canards de Limagne sont issus de souche Mulard, et sont nourris avec du maïs de la plaine de Limagne en Auvergne. Cette terre particulière de notre terroir donne un goût spécifique et unique à nos foies gras. Conditionné en bloc de 400gr, il se conserve 1 ans', 28.20, 'Bloc de foie gras de canard entier', 'blocfoie.jpg', true),
-(3, 2, 'Demi-Jambon Sec origine Auvergne, sans os et sans jarret, de 2.75 kg. Il se conserve 20 jours. A accompagner de vin rosé.', 43.15, 'Demi-Jambon Sec Auvergne', 'jambonsec.jpg', true),
-(3, 2, 'Jambon Sec des Combrailles, conditionné en paquet de 150 gr. Un vin rosé est le bienvenue pour déguster de jambon sec. Se conserve 75 jours.', 5.60, 'Jambon Sec', 'jambon.jpg', true),
-(3, 3, 'Saucisson sec aux figues, une des spécialités de notre exploitation. Dans la fabrication, outre la qualité des matières premières, le séchage donne au saucisson ses qualités gustatives.', 3.10, 'Saucisson aux figues', 'saucissonfigues.jpg', false),
-(3, 3, 'Saucisson aux herbes, spécialité de notre terroir. Le séchage est important mais la qualité des herbes de provence lui confère son parfum unique.', 3.25, 'Saucisson aux herbes', 'saucissonherbes.jpg', true),
-(5, 3, 'Saucisson aux noisettes a subit un séchage particulier en cave ce qui lui confère un gout incomparable. Les petits éclats de noisettes donne du croquant au saucisson', 4.85, 'Saucisson aux noisettes', 'saucissonnoisettes.jpg', true),
-(4, 3, 'Saucisse sèche primée au salon agricole 2012 de la région Rhône-Alpes pour son gout unique. En boyaux naturels, elle se conserve plus de 50 jours. Accompagner absolument de Velançay', 8.20, 'Saucisse seche', 'saucisseseche.jpg', true);
+(1, 4, 'Terrine de foie gras de canard. Se déguste avec un vin moelleux ou du beaujolais. Conditionné ainsi il se conserve 2 ans.', 19.50, 'Terrine de foie gras, 190gr', 'foiegras.jpg', true),
+(1, 4, 'Ce magret de canard de race Mulard est farci au foie gras. Il est idéal en salade, avec un vin moelleux. Il se conserve 40 jours.', 14.80, 'Magret de canard au foie gras (35%),150gr', 'magret.jpg', false),
+(2, 4, 'Nos rillettes de canard se dégustent avec un vin rouge léger. Elles contiennent 20% de foie gras. La conservation est de 30 jours.', 8.70, 'Rillettes de canard', 'rillettes.jpg', true),
+(1, 4, 'Foie gras de canard entier aux pépites de figues séchées. Les Canards de Limagne sont issus de souche Mulard, et sont nourris avec du maïs de la plaine de Limagne en Auvergne. Cette terre particulière de notre terroir donne un goût spécifique et unique à nos foies gras. Conditionné en bloc de 400gr, il se conserve 1 ans', 28.20, 'Bloc de foie gras de canard entier', 'blocfoie.jpg', true),
+(3, 5, 'Demi-Jambon Sec origine Auvergne, sans os et sans jarret, de 2.75 kg. Il se conserve 20 jours. A accompagner de vin rosé.', 43.15, 'Demi-Jambon Sec Auvergne', 'jambonsec.jpg', true),
+(3, 5, 'Jambon Sec des Combrailles, conditionné en paquet de 150 gr. Un vin rosé est le bienvenue pour déguster de jambon sec. Se conserve 75 jours.', 5.60, 'Jambon Sec', 'jambon.jpg', true),
+(3, 6, 'Saucisson sec aux figues, une des spécialités de notre exploitation. Dans la fabrication, outre la qualité des matières premières, le séchage donne au saucisson ses qualités gustatives.', 3.10, 'Saucisson aux figues', 'saucissonfigues.jpg', false),
+(3, 6, 'Saucisson aux herbes, spécialité de notre terroir. Le séchage est important mais la qualité des herbes de provence lui confère son parfum unique.', 3.25, 'Saucisson aux herbes', 'saucissonherbes.jpg', true),
+(5, 6, 'Saucisson aux noisettes a subit un séchage particulier en cave ce qui lui confère un gout incomparable. Les petits éclats de noisettes donne du croquant au saucisson', 4.85, 'Saucisson aux noisettes', 'saucissonnoisettes.jpg', true),
+(4, 6, 'Saucisse sèche primée au salon agricole 2012 de la région Rhône-Alpes pour son gout unique. En boyaux naturels, elle se conserve plus de 50 jours. Accompagner absolument de Velançay', 8.20, 'Saucisse seche', 'saucisseseche.jpg', true);
 
 
 -- ///////////////////////////////////////////////////////////////////////////////////////////////////
